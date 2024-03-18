72b24eb83853b4715d9599eca51d58aa4c041299
nhtY <nihat.yalcin@agu.edu.tr>
2024-03-18 13:18:00 +0300

    Create a RegistrationController.

---
+++ src/main/java/com/nihat/loginregister/controllers/RegistrationController.java
@@ -0,0 +1,20 @@
+package com.nihat.loginregister.controllers;
+
+import com.nihat.loginregister.registration.RegistrationRequest;
+import com.nihat.loginregister.registration.RegistrationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/registration")
+@RequiredArgsConstructor
+public class RegistrationController {
+
+    private RegistrationService registrationService;
+
+    public String register(@RequestBody RegistrationRequest request) {
+        return registrationService.register(request);
+    }
+}
