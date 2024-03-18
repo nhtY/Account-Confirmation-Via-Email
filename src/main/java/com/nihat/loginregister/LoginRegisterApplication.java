1545f1cfa5ed0330edbdcc302daf6f784f417ed3
nhtY <nihat.yalcin@agu.edu.tr>
2024-03-18 15:14:27 +0300

    Delete unused imports.

--- src/main/java/com/nihat/loginregister/LoginRegisterApplication.java
+++ src/main/java/com/nihat/loginregister/LoginRegisterApplication.java
@@ -1,15 +1,7 @@
 package com.nihat.loginregister;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 @SpringBootApplication
 public class LoginRegisterApplication {
