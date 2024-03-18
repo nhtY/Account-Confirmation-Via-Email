a42a867992d1b7f1ffd2cb3893def10b9c4a8fe0
nhtY <nihat.yalcin@agu.edu.tr>
2024-03-18 15:09:44 +0300

    Fix: add @PostMapping on the handler method

--- src/main/java/com/nihat/loginregister/controllers/RegistrationController.java
+++ src/main/java/com/nihat/loginregister/controllers/RegistrationController.java
@@ -2,8 +2,8 @@ package com.nihat.loginregister.controllers;
 
 import com.nihat.loginregister.registration.RegistrationRequest;
 import com.nihat.loginregister.registration.RegistrationService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +15,7 @@ public class RegistrationController {
 
     private final RegistrationService registrationService;
 
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
