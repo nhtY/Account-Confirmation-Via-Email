468c17a7cc47021d5213439e5da508155157ebcd
nhtY <nihat.yalcin@agu.edu.tr>
2024-03-18 12:23:08 +0300

    Implement AppUserService method: - implement loadUserByUsername method: - use email instead. - inject AppUserRepository to fetch the user. - set an exception message.

--- src/main/java/com/nihat/loginregister/appuser/AppUserService.java
+++ src/main/java/com/nihat/loginregister/appuser/AppUserService.java
@@ -1,12 +1,23 @@
 package com.nihat.loginregister.appuser;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class AppUserService implements UserDetailsService {
+
+    private final static String USER_NOT_FOUND_MSG = "User with email %s not found";
+
+    private final AppUserRepository appUserRepository;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        String.format(USER_NOT_FOUND_MSG, email)
+                ));
     }
 }
