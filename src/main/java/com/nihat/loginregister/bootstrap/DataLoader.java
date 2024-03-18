8e93cf8c68fc3c2cd3a12d4874aac6d39c4503c6
nhtY <nihat.yalcin@agu.edu.tr>
2024-03-18 15:10:55 +0300

    Add data loader to insert a user at the beginning.

---
+++ src/main/java/com/nihat/loginregister/bootstrap/DataLoader.java
@@ -0,0 +1,36 @@
+package com.nihat.loginregister.bootstrap;
+
+import com.nihat.loginregister.appuser.AppUser;
+import com.nihat.loginregister.appuser.AppUserRepository;
+import com.nihat.loginregister.appuser.AppUserRole;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataLoader {
+
+    private final AppUserRepository appUserRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public CommandLineRunner run() {
+        return args -> {
+            AppUser user = AppUser.builder()
+                    .name("name")
+                    .surname("surname")
+                    .email("email")
+                    .appUserRole(AppUserRole.USER)
+                    .enabled(true)
+                    .locked(false)
+                    .username("user")
+                    .password(passwordEncoder.encode("pass"))
+                    .build();
+
+            appUserRepository.save(user);
+        };
+    }
+}
