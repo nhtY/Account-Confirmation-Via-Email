56e8484a72755fdc588d4ce51e2ff7cd6d9e3604
nhtY <nihat.yalcin@agu.edu.tr>
2024-03-18 22:21:46 +0300

    Create ConfirmationTokenService: - generate token for a user. - save generated tokens.

---
+++ src/main/java/com/nihat/loginregister/registration/token/ConfirmationTokenService.java
@@ -0,0 +1,31 @@
+package com.nihat.loginregister.registration.token;
+
+import com.nihat.loginregister.appuser.AppUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ConfirmationTokenService {
+
+    private final static Integer VALID_TOKEN_DURATION_IN_MINUTE = 15;
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+
+    public void saveConfirmationToken(ConfirmationToken token) {
+        confirmationTokenRepository.save(token);
+    }
+
+    public ConfirmationToken generateToken(AppUser user) {
+        String token = UUID.randomUUID().toString();
+        return ConfirmationToken.builder()
+                .token(token)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(VALID_TOKEN_DURATION_IN_MINUTE))
+                .confirmedAt(null)
+                .appUser(user)
+                .build();
+    }
+}
