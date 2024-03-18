f3963aa70f23d4690e0abb71da48ed1171245b38
nhtY <nihat.yalcin@agu.edu.tr>
2024-03-18 15:20:04 +0300

    Use email validator in RegistrationService.

--- src/main/java/com/nihat/loginregister/registration/RegistrationService.java
+++ src/main/java/com/nihat/loginregister/registration/RegistrationService.java
@@ -1,11 +1,19 @@
 package com.nihat.loginregister.registration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RegistrationService {
 
+    private final EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
+        boolean isValidEmail = emailValidator.test(request.email());
+        if (!isValidEmail) {
+            throw new IllegalStateException("email not valid");
+        }
         return "works";
     }
 }
